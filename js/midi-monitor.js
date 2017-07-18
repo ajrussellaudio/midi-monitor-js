27a4510fd09db189ab79169e39601214fad3ef4e
Alan Russell <ajrussellaudio@gmail.com>
2017-07-18 01:19:47 +0100

    basic functionality

---
+++ js/midi-monitor.js
@@ -0,0 +1,35 @@
+function onMIDISuccess(midi) {
+  const inputs = midi.inputs.values();
+
+  for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
+    input.value.onmidimessage = onMIDIMessage;
+  }
+}
+
+function onMIDIFailure(e) {
+  console.log("No access to MIDI devices.")
+}
+
+function onMIDIMessage(message) {
+  const data = message.data;
+  if (data[0] === 128) noteOff(data[1]);
+  if (data[0] === 144) noteOn(data[1], data[2]);
+}
+
+function noteOn(noteNumber, velocity) {
+  console.log("Note ON:", noteToString(noteNumber), "velocity:", velocity)
+}
+
+function noteOff(noteNumber) {
+  console.log("Note OFF:", noteToString(noteNumber));
+}
+
+window.onload = () => {
+  if (navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess({
+      sysex: false
+    }).then(onMIDISuccess, onMIDIFailure);
+  } else {
+    alert("No MIDI support in your browser!");
+  }
+}
---
+++ js/note-convert.js
@@ -0,0 +1,20 @@
+function noteToString(midi) {
+  return pitch(midi) + octave(midi)
+}
+
+function pitch(midi) {
+  return pitchClass(degree(midi));
+}
+
+function degree(midi) {
+  return midi % 12;
+}
+
+function octave(midi) {
+  return Math.floor(midi / 12) - 2;
+}
+
+function pitchClass(degree) {
+  const pitchClasses = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+  return pitchClasses[degree];
+}
