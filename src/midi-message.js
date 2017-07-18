7b9fffc8c6e6a0f805bc06415fa11c492b98d17d
Alan Russell <ajrussellaudio@gmail.com>
2017-07-18 20:34:40 +0100

    abstract out midi message

---
+++ src/midi-message.js
@@ -0,0 +1,25 @@
+import noteToString from './note-convert.js';
+
+export default class MIDIMessage {
+
+  constructor(message) {
+    this.status = message.data[0];
+    this.data1 = message.data[1];
+    this.data2 = message.data[2];
+  }
+
+  command() {
+    const commands = ["Note Off", "Note On", "Poly Pressure", "Control Change", "Program Change", "Mono Pressure", "Pitch Bend", "System" ];
+    const index = (this.status - 128) / 16;
+    return commands[index];
+  }
+
+  getInfo() {
+    return {
+      command: this.command(),
+      _status: this.status,
+      _data1: this.data1,
+      _data2: this.data2
+    }
+  }
+}
--- src/midi-monitor.js
+++ src/midi-monitor.js
@@ -1,4 +1,5 @@
-import noteToString from './note-convert.js';
+
+import MIDIMessage from './midi-message.js';
 
 function onMIDISuccess(midi) {
   const inputs = midi.inputs.values();
@@ -12,17 +13,13 @@ function onMIDIFailure(e) {
 }
 
 function onMIDIMessage(message) {
-  const data = message.data;
-  if (data[0] === 128) noteOff(data[1]);
-  if (data[0] === 144) noteOn(data[1], data[2]);
+  const note = new MIDIMessage(message);
+  console.log(note.getInfo());  
 }
 
-function noteOn(noteNumber, velocity) {
-  console.log("Note ON:", noteToString(noteNumber), "velocity:", velocity)
-}
-
-function noteOff(noteNumber) {
-  console.log("Note OFF:", noteToString(noteNumber));
+function isNoteStatus(message) {
+  const status = message.data[0];
+  return status === 144 || status === 128;
 }
 
 window.onload = () => {
