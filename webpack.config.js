9c1f304d7a810e89882e8527881f77d34a722512
Alan Russell <ajrussellaudio@gmail.com>
2017-07-18 15:38:15 +0100

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/midi-monitor.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "MIDI Monitor"
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
