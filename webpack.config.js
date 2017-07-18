fe64c0405cd6a519ae571411dddf5d2aa302e163
Alan Russell <ajrussellaudio@gmail.com>
2017-07-18 15:48:02 +0100

    add clean plugin and dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/midi-monitor.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: "MIDI Monitor"
     })
